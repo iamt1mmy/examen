7af76dcc2627b1c9e3e5534f655a9b7270f37f7a
TIMMY <timotei.adam@emanuel.ro>
2024-01-22 09:25:00 +0200

    mutare fisier in folder

---
+++ examenp1/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int YearOfBirth { get; set; }
+
+    public int Age
+    {
+        get
+        {
+            return DateTime.Now.Year - this.YearOfBirth;
+        }
+    }
+}
+
+public static class PersonProcessor
+{
+    public static Person GetOldestPerson(Person[] people)
+    {
+        if (people == null || people.Length == 0)
+        {
+            return null;
+        }
+        Person oldestPerson = people[0];
+        foreach (Person person in people)
+        {
+            if (person.Age > oldestPerson.Age)
+            {
+                oldestPerson = person;
+            }
+        }
+        return oldestPerson;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Numarul de persoane: ");
+        int numarPersoane = int.Parse(Console.ReadLine());
+
+        Person[] persoane = new Person[numarPersoane];
+
+        for (int i = 0; i < numarPersoane; i++)
+        {
+            Console.WriteLine($"Introduceti informatii pentru persoana {i + 1}:");
+            Console.Write("Prenume: ");
+            string prenume = Console.ReadLine();
+
+            Console.Write("Nume de familie: ");
+            string nume = Console.ReadLine();
+
+            Console.Write("Anul nasterii: ");
+            int anNastere = int.Parse(Console.ReadLine());
+
+            persoane[i] = new Person
+            {
+                FirstName = prenume,
+                LastName = nume,
+                YearOfBirth = anNastere
+            };
+        }
+        Person ceaMaiInVarsta = PersonProcessor.GetOldestPerson(persoane);
+
+        Console.WriteLine($"\nPersoana cu cea mai mare varsta este:");
+        Console.WriteLine($"Prenume: {ceaMaiInVarsta.FirstName}");
+        Console.WriteLine($"Nume de familie: {ceaMaiInVarsta.LastName}");
+        Console.WriteLine($"Anul nasterii: {ceaMaiInVarsta.YearOfBirth}");
+        Console.WriteLine($"Varsta: {ceaMaiInVarsta.Age} ani");
+    }
+}
